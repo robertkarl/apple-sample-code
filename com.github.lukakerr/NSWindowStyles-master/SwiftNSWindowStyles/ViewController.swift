5073f04b3742561ae1eb9fd503a9827aadf49da8
Robert Karl <robertkarl@users.noreply.github.com>
2020-12-13 17:54:26 -0800

    Add a lukakerr and  raywenderlich sample; make them compile

---
+++ com.github.lukakerr/NSWindowStyles-master/SwiftNSWindowStyles/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftNSWindowStyles
+//
+//  Created by Luka Kerr on 4/3/18.
+//  Copyright © 2018 Luka Kerr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    let x = "as🤑df"
+    let 🔥 = "fire"
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+}
